4e4a0698bad87faa5673c1d113f1a549383d759a
kartoFlane <tbachminski@gmail.com>
2019-12-28 21:00:50 +0100

    Fix internal mission variable being out-of-date sometimes

--- modApiExt/alter.lua
+++ modApiExt/alter.lua
@@ -553,7 +553,7 @@ modApiExtHooks.missionUpdate = function(mission)
 	-- the missionUpdate hook.
 	-- Set it here, in case we load into a game in progress (missionStart
 	-- is not executed then)
-	if not modApiExt_internal.mission and mission then
+	if mission then
 		modApiExt_internal.mission = mission
 	end
 	if Board and not Board.gameBoard then
